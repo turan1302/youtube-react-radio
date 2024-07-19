56d6a656304b0aaf2ca7a072cf87b1e8fd1eebc8
turan1302 <printwordex@gmail.com>
2024-07-19 14:46:35 +0300

    mobx kurulumu gerceklestirildi

---
+++ radio_frontend/src/store/AuthStore.js
@@ -0,0 +1,52 @@
+import cryptoJS from "crypto-js";
+import {jwtDecode} from "jwt-decode";
+import sign from "jwt-encode";
+import {action, makeAutoObservable, observable} from "mobx";
+
+class AuthStore{
+    appState = null;
+
+    constructor() {
+        makeAutoObservable(this,{
+            appState : observable,
+            saveToken : action,
+            getToken : action,
+            removeToken : action
+        })
+    }
+
+    saveToken = (appState)=>{
+        try {
+            localStorage.setItem("appState",cryptoJS.AES.encrypt(sign(appState,"secret"),"radio").toString());
+            this.appState = appState;
+        }catch (e) {
+            console.log(e);
+        }
+    }
+
+    getToken = ()=>{
+        try {
+            const appStateData = localStorage.getItem("appState");
+            if (appStateData){
+                const bytes = cryptoJS.AES.decrypt(appStateData,"radio");
+                const originalText = bytes.toString(cryptoJS.enc.Utf8);
+                this.appState = jwtDecode(originalText);
+            }else{
+                this.appState = null;
+            }
+        }catch (e) {
+            console.log(e);
+        }
+    }
+
+    removeToken = ()=>{
+        try {
+            localStorage.removeItem("appState");
+            this.appState = null;
+        }catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+export default new AuthStore();
